652808c2f6633ae8cb23fae935fc63d121dca295
aymen merad <ay28mene@gmail.com>
2025-04-13 00:44:51 +0100

    update to let frontend send req

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from typing import Union
 from fastapi import FastAPI
 from chat_bot import generate
 from schemas import chat_history,chat_output, chat_input
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
